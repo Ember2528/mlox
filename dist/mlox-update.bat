ae88d3a4e294bb4db0b8c4377c6277f27879c2ff
rfuzzo <r.fuzzo@gmail.com>
2022-06-12 16:25:31 +0200

    Update mlox-update.bat

--- dist/mlox-update.bat
+++ dist/mlox-update.bat
@@ -36,38 +36,4 @@ if "%curversion%"=="%version%" (
 	curl -L https://github.com/rfuzzo/mlox/releases/latest/download/mlox.exe -o mlox.exe
 )
 
-
-
-
-
-rem Update mlox-rules
-echo Updating mlox-rules ...
-
-rem create ./mlox/ directory
-if not exist mlox\NUL mkdir mlox
-
-rem check if ./mlox/ is a git repository
-if exist mlox/.git goto ISGIT else goto ISNOTGIT
-
-:ISNOTGIT
-rem Clone just the repository's .git folder excluding files as they are already in `mlox` into an empty temporary directory
-rem see https://stackoverflow.com/a/2484349/16407587
-git clone --no-checkout https://github.com/DanaePlays/mlox-rules.git mlox/mlox.tmp 
-
-rem Move the .git folder to the directory with the files.
-rem This makes `mlox` a git repo.
-robocopy mlox/mlox.tmp mlox /E /MOVE /NFL /NDL
-
-rem git thinks all files are deleted, this reverts the state of the repo to HEAD.
-rem WARNING: any local changes to the files will be lost.
-cd mlox
-git reset --hard HEAD
-cd ..
-
-:ISGIT
-rem fetch and fast-forward pull mlox-rules
-cd mlox
-git fetch
-git pull --ff-only
-
 pause
